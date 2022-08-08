a558bb9421354c84f8b99dc585534b7eb0f365b3
yadukrishnan.d <yadukrishnan.d@gmail.com>
2022-08-07 22:22:32 -0400

    Login testcase one is added

---
+++ src/test/java/com/reshmyhome/LoginTestCases.java
@@ -0,0 +1,12 @@
+package com.reshmyhome;
+
+import org.junit.Test;
+
+public class LoginTestCases {
+
+    @Test
+    public void loginTestOne(){
+        System.out.println("logged in");
+    }
+
+}
