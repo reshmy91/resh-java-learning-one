7704ee3f6cfc752334e7ec7bc4a4cda1feb8bbeb
yadukrishnan.d <yadukrishnan.d@gmail.com>
2022-08-07 22:32:26 -0400

    three testcases are added

--- src/test/java/com/reshmyhome/LoginTestCases.java
+++ src/test/java/com/reshmyhome/LoginTestCases.java
@@ -5,7 +5,22 @@ import org.junit.Test;
 public class LoginTestCases {
 
     @Test
-    public void loginTestOne(){
+    public void loginTestOne() {
+        System.out.println("logged in");
+    }
+
+    @Test
+    public void loginTestTwo() {
+        System.out.println("logged in");
+    }
+
+    @Test
+    public void loginTestThree() {
+        System.out.println("logged in");
+    }
+
+    @Test
+    public void loginTestFour() {
         System.out.println("logged in");
     }
 
